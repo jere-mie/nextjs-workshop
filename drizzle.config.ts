e6b12f723e7f0f8d9fae6ae06476e30f55840920
jere-mie <jeremiedevelops@gmail.com>
2026-02-06 20:39:13 -0500

    make db work with turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,9 @@ config({ path: ".env" });
 export default defineConfig({
     schema: "./db/schema.ts",
     out: "./migrations",
-    dialect: "sqlite",
+    dialect: "turso",
     dbCredentials: {
         url: process.env.DB_URL!,
+        authToken: process.env.DB_AUTH_TOKEN!,
     },
 });
